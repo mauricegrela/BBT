ad53918646d6a97739da85ae26c1d2ac8c794427
Maurice Grela <maurice.grela@gmail.com>
2019-01-31 16:09:36 -0800

    Committing new script for interpreting 2D mouse clicks into 3d space.

---
+++ Assets/Scripts/Camera/Camera_Pointing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Pointing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("wokring");
+                hit.collider.gameObject.GetComponent<SpriteRotation>().Throw();
+                //hit.collider.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                /*if (hit.collider != null)
+                {
+                    hit.collider.enabled = false;
+                }*/
+            }
+        }
+	}
+}
