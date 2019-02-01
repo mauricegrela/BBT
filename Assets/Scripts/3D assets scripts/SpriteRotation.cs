cebafbab46067a72dc9a41d90094a085e06d13b5
Maurice Grela <maurice.grela@gmail.com>
2019-01-31 16:08:49 -0800

    Committing first draft of the physics simulation for tapping environmental objects.

--- Assets/Scripts/3D assets scripts/SpriteRotation.cs
+++ Assets/Scripts/3D assets scripts/SpriteRotation.cs
@@ -12,10 +12,26 @@ public class SpriteRotation : MonoBehaviour {
         // the second argument, upwards, defaults to Vector3.up
         Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
         transform.rotation = rotation;*/
+
+        gameObject.AddComponent<Rigidbody>();
+        gameObject.AddComponent<BoxCollider>();
+
+        gameObject.GetComponent<BoxCollider>().size = new Vector3(gameObject.GetComponent<BoxCollider>().size.x,gameObject.GetComponent<BoxCollider>().size.y,0.001f);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Throw()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        float speed = 60;
+        //gameObject.AddComponent<Rigidbody>().isKinematic = false;
+        Vector3 force = transform.forward;
+        force = new Vector3(force.x, 1, force.z);
+        gameObject.GetComponent<Rigidbody>().AddForce(force * speed);
+    }
 }
