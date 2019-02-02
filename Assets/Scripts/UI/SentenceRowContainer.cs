7e0e1f7cf032b3d99e900449a9010988fff0e852
Maurice Grela <maurice.grela@gmail.com>
2019-02-01 18:11:52 -0800

    committing updates to the highlighting algorithm,

--- Assets/Scripts/UI/SentenceRowContainer.cs
+++ Assets/Scripts/UI/SentenceRowContainer.cs
@@ -123,10 +123,10 @@ public class SentenceRowContainer : MonoBehaviour
         foreach (WordText text in texts)
         {
 			//Debug.Log(text.text);
-            if (text.wordGroup == wordGroup && PageManagerRefScript.IsReadingAlong == 1.0f)
+            if (text.wordGroup == wordGroup )//&& PageManagerRefScript.IsReadingAlong == 1.0f)
             {
 				text.text.color = HighlightedColor;
-               //Debug.Log(text.text.text);
+                Debug.Log(text.text.text);
             }
 	            else
 	            {
