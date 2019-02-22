84cf7ed830a5d7d40233d10f31ddf917d0495c67
Maurice Grela <maurice.grela@gmail.com>
2019-02-22 14:49:16 -0800

    Reimplementing the IAP

---
+++ Assets/Scripts/IAP/InAppPurchaseScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InAppPurchaseScript : MonoBehaviour {
+
+    public GameObject[] Characters;
+    public int CharactersUnlocked;
+    public GameObject LockedCharacter;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public void ActivateCharacters()
+    {
+
+
+        for (int i = 0 ; i <= Characters.Length-1; i++)
+        {
+
+            if(i<CharactersUnlocked)
+            {
+                //Characters[i].SetActive(true);
+                Characters[i].GetComponent<Image>().enabled = false;
+                Characters[i].GetComponent<Button>().enabled = false;
+            }
+                else
+                {
+                Characters[i].GetComponent<Image>().enabled = true;
+                Characters[i].GetComponent<Button>().enabled = true;
+                }
+
+            //Debug.Log(Characters[i].GetComponent<Image>().enabled);
+        }
+        if(CharactersUnlocked == 2)
+        {
+            LockedCharacter.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/Scripts/IAP/MyStoreClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Purchasing;
+using UnityEngine;
+
+public class MyStoreClass : MonoBehaviour, IStoreListener
+{
+    void Awake()
+    {
+        ConfigurationBuilder builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("com.imaginecreatemedia.daisy.StickerSet", ProductType.NonConsumable);
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions) { }
+    public void OnInitializeFailed(InitializationFailureReason error) { }
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e) { return PurchaseProcessingResult.Complete; }
+    public void OnPurchaseFailed(Product item, PurchaseFailureReason r) { }
+}
