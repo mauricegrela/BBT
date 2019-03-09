58c32cbf9d4c14b88c346da89c9d17bf32eea2e3
Maurice Grela <maurice.grela@gmail.com>
2019-03-08 18:30:05 -0800

    Implementing Swipymenu

---
+++ Assets/SwipyMenu/Scripts/SwipyMenuScrollRect.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This is just a ScrollRect Wrapper to restrict ScrollRect`s properties that cant be used with SwipyMenu in Editor. So no code here. Check SwipyMenuScrollRectEditor.
+/// </summary>
+public class SwipyMenuScrollRect : ScrollRect {	}
