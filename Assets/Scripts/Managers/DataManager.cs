24474dc323e12938b81d9c051437be4b6b7c9642
Maurice Grela <maurice.grela@gmail.com>
2019-03-11 19:46:26 -0700

    Committing fixes to the english scripts of both little people and kalkalilh.

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -11,7 +11,7 @@ using System.Reflection.Emit;
 public class DataManager
 {
     public static string currentLanguage = "english";
-    public static string currentStoryName = "sasquatch";
+    public static string currentStoryName = "littlepeople";
     public static bool isINISet = false;
     public static StoryObject currentStory;
     public static string[] languageManager;
