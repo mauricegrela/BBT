a44c37a848abc251de5c2c4348343b755fa6cfa7
Maurice Grela <maurice.grela@gmail.com>
2019-03-20 20:33:22 -0700

    commenting

--- Assets/Scripts/Animation/ImageFadeOut.cs
+++ Assets/Scripts/Animation/ImageFadeOut.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ImageFadeOut : MonoBehaviour {
-
-    private void Start()
-    {
-
-    }
-
-    void Update()
-    {
-       
-    }
-
-
+    
     public void StartCo()
     {
         StartCoroutine(FadeTo(0.0f, 1.5f));
-        Debug.Log("working");
     }
+    
     IEnumerator FadeTo(float aValue, float aTime)
     {
-        
         float alpha = GetComponent<SpriteRenderer>().color.a;
         for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
         {
