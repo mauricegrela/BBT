5ae22589c2bd932bc8c0c0bc632254bd48496cf4
Yael Sagi <yaelsagi@Yaels-MacBook-Pro.local>
2019-05-27 21:46:49 +0300

    watercolor effect follows text position

---
+++ Assets/Under text-watercolor effect/UnderTextEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderTextEffect : MonoBehaviour {
+
+    GameObject TextContainer;
+	void Start () 
+    {
+        TextContainer = GameObject.FindGameObjectWithTag("TextPlacement");
+        gameObject.transform.position = TextContainer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
