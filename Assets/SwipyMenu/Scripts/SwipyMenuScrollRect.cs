6e422d053edf3d60aab610d1d7ea9f6055bcfc95
Yael Sagi <yaelsagi@Yaels-MacBook-Pro.local>
2019-05-30 21:30:43 +0300

    Bye bye swipy menu

--- Assets/SwipyMenu/Scripts/SwipyMenuScrollRect.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-/// <summary>
-/// This is just a ScrollRect Wrapper to restrict ScrollRect`s properties that cant be used with SwipyMenu in Editor. So no code here. Check SwipyMenuScrollRectEditor.
-/// </summary>
-public class SwipyMenuScrollRect : ScrollRect {	}
