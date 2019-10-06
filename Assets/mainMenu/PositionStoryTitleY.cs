49d145eb2b22ef8b20411cdd56bc5c8b90c8dbf3
Yael Sagi <yaelsagi@Yaels-MacBook-Pro.local>
2019-10-06 17:44:45 +0300

    Chepter select updated
    Chepter select updated

---
+++ Assets/mainMenu/PositionStoryTitleY.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionStoryTitleY : MonoBehaviour {
+
+    public RectTransform OtherRectTransform;
+    Transform rectTransform;
+    Vector3 pos;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        pos = rectTransform.position;
+    }
+    void Update () 
+    {
+        pos.y = OtherRectTransform.position.y;
+        rectTransform.position = pos;
+    }
+}
