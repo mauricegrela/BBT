60de58f95bc125bcbc7ed08669b96b97ac7563be
Yael Sagi <yaelsagi@Yaels-MacBook-Pro.local>
2019-10-17 12:05:09 +0300

    some little things

--- Assets/mainMenu/PositionStoryTitleY.cs
+++ Assets/mainMenu/PositionStoryTitleY.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PositionStoryTitleY : MonoBehaviour {
 
-    public RectTransform OtherRectTransform;
-    Transform rectTransform;
-    Vector3 pos;
 
+    private RectTransform rectTrans;
+    //Vector2 pivot;
     private void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
-        pos = rectTransform.position;
+        rectTrans = GetComponent<RectTransform>();
+        //pos = rectTransform.position;
+        //rect.position = Camera.main.ScreenToWorldPoint(new Vector2(rect.position.x, Screen.height / 2));
+
+        rectTrans.position = new Vector2(rectTrans.position.x, Screen.height/2);
+
+        //rectTrans.pivot = new Vector2(rectTrans.rect.width / 2, rectTrans.rect.height/2); //center of the rect
+        //Vector2 size = rectTrans.rect.size;
+        //Vector2 deltaPivot = rectTrans.pivot - pivot;
+        //Vector3 deltaPosition = new Vector3(deltaPivot.x * size.x, deltaPivot.y * size.y);
+        //rectTrans.pivot = pivot;
+
+        //rectTrans.localPosition -= deltaPosition;
+
+        //rect.position = new Vector2(rect.position.x, rect.rect.height / 2);
+
+        //rect.position= new Vector2(rect.position.x, rect.rect.height/2);
+        //rect.position = new Vector2(rect.position.x, Screen.height/2- rect.rect.height / 2);// good for iphon x
+
+        //rect.
     }
-    void Update () 
-    {
-        pos.y = OtherRectTransform.position.y;
-        rectTransform.position = pos;
-    }
+    //void Update () 
+    //{
+    //    pos.y = OtherRectTransform.position.y;
+    //    rectTransform.position = pos;
+    //}
 }
